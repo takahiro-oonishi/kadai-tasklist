8b6dd6d617dd8d2e6fc3a47f489c328da2dd4931
takahiro.oonishi <ve.wak.ld@gmail.com>
2020-01-31 08:16:57 +0000

    提出

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,8 +26,13 @@
         </header>
         
         <div class="container">
+            @include('commons.error_messages')
+            
             @yield('content')
         </div>
+        
+        
+        
         
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
